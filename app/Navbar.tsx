30d9a2da84e4b4ca22efa0e2804738b2ab8d0442
KonstantinStanmeyer <konstantin@stanmeyer.com>
2024-10-08 16:24:58 -0400

    pixel camera and random message fix

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -18,8 +18,8 @@ export default function Navbar({ user }: Props){
     
     return (
         <div className="bg-[#ffdeb9] w-full h-6 border-black border-b-[1.5px] flex flex-row">
-            <a className="flex flex-row border-black border-r-[1.5px] pr-2" href="/">
-                <img className="ml-2" src="" />
+            <a className="flex flex-row items-center border-black border-r-[1.5px] pr-2" href="/">
+                <img className="ml-2 h-4" src="/pixel-camera.png" />
                 <p className="text-base pixelify ml-2 hidden md:block">{loginMessage[randomIndex]}</p>
                 <p className="text-base pixelify ml-2 block md:hidden">Sign in</p>
             </a>
